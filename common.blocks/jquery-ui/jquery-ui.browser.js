ceecedc9b1b6bbaab985149bd70626ff2a3b1b68
voischev <voischev.ivan@ya.ru>
2014-09-03 15:21:24 +0400

    upd

--- common.blocks/jquery-ui/jquery-ui.browser.js
+++ common.blocks/jquery-ui/jquery-ui.browser.js
@@ -2,12 +2,9 @@ modules.define(
     'jquery-ui',
     ['loader_type_js', 'jquery'],
     function(provide, loader, $) {
-
-        function doProvide(preserveGlobal) {
-            provide(preserveGlobal? $.ui : $.ui);
-        }
-
         typeof $.ui !== 'undefined'?
-            doProvide(true) :
-            loader('//yastatic.net/jquery-ui/1.11.1/jquery-ui.min.js', doProvide);
+            provide($.ui) :
+            loader(
+                '//yastatic.net/jquery-ui/1.11.1/jquery-ui.min.js',
+                function() { provide($.ui); });
 });
